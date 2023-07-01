f32a4625f2855530e6e0334a500e195d9f33076d
Ben Jones <benpaddlejones@outlook.com>
2023-07-01 22:01:17 +1000

    add structure - no content yet

---
+++ Seeed Sensor Library/Biomedical/Finger_Strap_Hear_Rate_Sensor/blank.ino
@@ -0,0 +1,17 @@
+/*
+  Connect to digital pin 2
+  https://wiki.seeedstudio.com/Grove-Red_LED/
+*/
+
+void setup() {
+  // initialize digital pin2  as an output.
+  pinMode(2, OUTPUT);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  digitalWrite(2, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(1000);                       // wait for a second
+  digitalWrite(2, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000);                       // wait for a second
+}
