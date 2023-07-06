c0a0f89ac7a602064208abee13e52fe9834b22ce
Ben Jones <benpaddlejones@outlook.com>
2023-07-06 12:14:00 +1000

    add doc link

--- Ardunio_Bootcamp/01.serialMonitor/01.serialMonitor.ino
+++ Ardunio_Bootcamp/01.serialMonitor/01.serialMonitor.ino
@@ -8,6 +8,8 @@
   Contact: 
 */
 
+https://docs.arduino.cc/software/ide-v2/tutorials/ide-v2-serial-plotter
+
 // The setup function runs once when you press reset or power the board
 void setup() {
 
