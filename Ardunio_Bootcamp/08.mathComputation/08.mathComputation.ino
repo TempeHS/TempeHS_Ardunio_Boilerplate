e5161533853ba15c44648315c5421db83a6e9c4d
Ben Jones <benpaddlejones@outlook.com>
2023-07-08 22:39:42 +1000

    update ino

--- Ardunio_Bootcamp/08.mathComputation/08.mathComputation.ino
+++ Ardunio_Bootcamp/08.mathComputation/08.mathComputation.ino
@@ -7,19 +7,24 @@
     4. I can access the order of operations documentation and use parenthesis to control the order of operations
     5. I understand short-hand incrementation and decrementation 
     6. I can seed and generate a random number
+    7. I can map an analogue value on a range of 0-1023 to a PWM value on a range of 0-255
 
   Student Notes:
-    % ???
-    * ???
-    + ???
-    - ???
-    / ???
-    = ???
+    % (remainder)
+    * (multiplication)
+    + (addition)
+    - (subtraction)
+    / (division)
+    = (assignment operator)
 
   Documentation: 
     https://www.arduino.cc/reference/en/#structure
     https://en.cppreference.com/w/cpp/language/operator_precedence
     https://www.arduino.cc/reference/en/language/functions/random-numbers/random/
+    https://www.arduino.cc/reference/en/language/functions/math/map/
+  Schematic:
+      https://www.tinkercad.com/things/ihcHlUXS9Y7?sharecode=bOdn0Sm9OuCJ6qI9GAwdyLsZDEUcecsmUharAUXXRIQ
+      https://github.com/TempeHS/TempeHS_Ardunio_Boilerplate/blob/main/Ardunio_Bootcamp/08.mathComputation/Bootcamp-MAP-PotLED.png
 */
 
 void setup() {
