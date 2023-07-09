cc1a4cf4a253ac6de326a508c4f80866c9f8c717
Ben Jones <benpaddlejones@outlook.com>
2023-07-09 22:31:10 +1000

    Update 12.mF.motorShield.ino

--- Ardunio_Bootcamp/12.motorFundamentals/12.mF.motorShield/12.mF.motorShield.ino
+++ Ardunio_Bootcamp/12.motorFundamentals/12.mF.motorShield/12.mF.motorShield.ino
@@ -1,51 +1,26 @@
 /*
   Author: 
-  Learning Intention: The students will learn how to connect and control a continuous 360-degree servo.
+  Learning Intention: The students will learn how to wire two motors to a H-Bridge controller and control the motors speed and direction.
   Success Criteria:
-    1. I understand how to power the Servo externally from the Arduino
-    2. I understand the benefits of powering a motor externally
-    2. I can manually write different directions and speeds to two servos
+    1. I understand what a H bidge is and what it does
+    2. I can fit the shield correctly to teh Arduino
+    2. I can manually write different directions and speeds to two motors
     3. I understand the advantages and disadvantages of a continuous servo over a motor and H bridge
 
   Student Notes: 
 
   Documentation: 
-    https://github.com/arduino-libraries/Servo <-- We are still using this library
+    https://docs.arduino.cc/tutorials/motor-shield-rev3/msr3-controlling-dc-motor
 
   Schematic:
-    https://github.com/TempeHS/TempeHS_Ardunio_Boilerplate/blob/main/Ardunio_Bootcamp/12.motorFundamentals/12.mF.continuousServoMotor/Bootcamp-continuousServoMotor.png
+    https://github.com/TempeHS/TempeHS_Ardunio_Boilerplate/blob/main/Ardunio_Bootcamp/12.motorFundamentals/12.mF.motorShield/12.mF.motorShield.png
 */
-
-#include <Servo.h>
-
-Servo leftMotor;  // create servo object to control the left continuous servo
-Servo rightMotor;  // create servo object to control the right continuous servo
 
 void setup() {
     // Associate the control pin with each motor
-  leftMotor.attach(2);
-  rightMotor.attach(4);
 
-  // Set both servos to stationary.
-  leftMotor.write(90);
-  rightMotor.write(90);
 }
 
 void loop() {
-    // Servo spins forward at full speed for 3 seconds.
-  leftMotor.write(180);
-  rightMotor.write(0);
-  delay(3000);
-  // Servo is stationary for 1 seconds.
-  leftMotor.write(90);
-  rightMotor.write(90);
-  delay(1000);
-  // Servo spins in reverse at full speed for 3 seconds.
-  leftMotor.write(0);
-  rightMotor.write(180);
-  delay(3000);
-  // Servo is stationary for 13 seconds.
-  leftMotor.write(90);
-  rightMotor.write(90);
-  delay(1000);
+
 }
