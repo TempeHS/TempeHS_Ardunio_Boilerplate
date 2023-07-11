94a6c406e917b72cc288847040382d20d9e2cb1e
Ben Jones <benpaddlejones@outlook.com>
2023-07-11 20:07:43 +1000

    add OOP to bootcamp

--- Ardunio_Bootcamp/07.mathComputation/07.mathComputation.ino
+++ Ardunio_Bootcamp/07.mathComputation/07.mathComputation.ino
@@ -1,30 +1,21 @@
 /*
   Author: 
   Learning Intention: The students will learn basic math and arithmetic operations
-    1. I know the arithmetic operation symbols %, *, +, -, / & =
-    2. I can perform arithmetic calculations
-    3. I know how to access more information in the documentation about math and arithmetic
-    4. I can access the order of operations documentation and use parenthesis to control the order of operations
-    5. I understand short-hand incrementation and decrementation 
-    6. I can seed and generate a random number
-    7. I can map an analogue value on a range of 0-1023 to a PWM value on a range of 0-255
+    1. I understand how a function is called
+    2. I can break a simple program like LED blick into functions that I can be re-use
+    3. I know the computation term 'abstraction.'
+    4. I can create separate my functions into functions.ino files to make my code more manageable
+    5. I can return values from a function and use the value in my code
+    6. I can pass values into my functions to make them more secure
+    7. I know the computation term encapsulation
 
   Student Notes:
-    % (remainder)
-    * (multiplication)
-    + (addition)
-    - (subtraction)
-    / (division)
-    = (assignment operator)
-
+    
   Documentation: 
-    https://www.arduino.cc/reference/en/#structure
-    https://en.cppreference.com/w/cpp/language/operator_precedence
-    https://www.arduino.cc/reference/en/language/functions/random-numbers/random/
-    https://www.arduino.cc/reference/en/language/functions/math/map/
+    https://www.youtube.com/watch?v=mHa1mUd1Kmg
+
   Schematic:
-      https://www.tinkercad.com/things/ihcHlUXS9Y7?sharecode=bOdn0Sm9OuCJ6qI9GAwdyLsZDEUcecsmUharAUXXRIQ
-      https://github.com/TempeHS/TempeHS_Ardunio_Boilerplate/blob/main/Ardunio_Bootcamp/08.mathComputation/Bootcamp-MAP-PotLED.png
+      
 */
 
 void setup() {
---
+++ Ardunio_Bootcamp/14.OPP-Object_Oriented_Programming/14.OPP-Object_Oriented_Programming.ino
@@ -0,0 +1,21 @@
+/*
+  Author: 
+  Learning Intention: The students will learn basic math and arithmetic operations
+    1. 
+
+  Student Notes:
+    
+  Documentation: 
+    https://www.youtube.com/watch?v=S_uaROFnWSg
+    https://youtu.be/cUVryWbVkXk
+    
+  Schematic:
+*/
+
+void setup() {
+
+}
+
+void loop() {
+
+}
