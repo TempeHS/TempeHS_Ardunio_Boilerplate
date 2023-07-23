d09155cd5cc4e8d0ba01bdb3e93a98a9009bc08b
Ben Jones <benpaddlejones@outlook.com>
2023-07-23 16:33:37 +1000

    improve MQ9 notes

--- TempeHS_Sensor_Catalogue/Environment/Gas_Sensor_MQ9/Gas_Sensor_MQ9.ino
+++ TempeHS_Sensor_Catalogue/Environment/Gas_Sensor_MQ9/Gas_Sensor_MQ9.ino
@@ -1,9 +1,14 @@
 /*
-  Purpose: An sensor that detects the presence of Carbon Monoxide, Coal Gas, Liquefied Gas
+  Purpose: A sensor that detects the presence of Carbon Monoxide, Coal Gas, Liquefied Gas.
   Notes: 
-    Analogue Sensor
-    The output voltage from the Gas sensor increases when the concentration of gas increases. Sensitivity can be adjusted by varying the potentiometer.
-  Author: Ben Jones ??/7/23
+    Analogue or Digital Sensor
+    As an analogue sensor the output voltage from the Gas sensor increases when the concentration of gas increases.
+    As a digital sensor the pin out will output low for no gas present or high for gas present.
+    Sensitivity can be adjusted by varying the potentiometer.
+    The sensor does not report the differnet gases 
+    When you first use the sensor after it has been stored for a long time (a month or more), it needs to be warmed up for 24-48 hours to ensure it works accurately.
+    To warm up a MQ sensor, just connect its VCC and GND pins to a power supply or arduino.
+  Author: Ben Jones 23/7/23
   Contact: benjmain.jones21@det.nsw.edu.au
   Source: https://wiki.seeedstudio.com/Grove-Gas_Sensor-MQ9/
 */
