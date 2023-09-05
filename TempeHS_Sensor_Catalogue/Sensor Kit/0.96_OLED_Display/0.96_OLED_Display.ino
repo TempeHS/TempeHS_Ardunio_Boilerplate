5e07fe3e07d1190f07c6d0c485c275aed72e79b8
Ben Jones <benpaddlejones@outlook.com>
2023-09-05 16:14:58 +1000

    update oled library

--- TempeHS_Sensor_Catalogue/Sensor Kit/0.96_OLED_Display/0.96_OLED_Display.ino
+++ TempeHS_Sensor_Catalogue/Sensor Kit/0.96_OLED_Display/0.96_OLED_Display.ino
@@ -23,9 +23,10 @@ void setup(void) {
 }
 
 void loop(void) {
-  u8g2.clearBuffer();                   // clear the internal memory
-  u8g2.setFont(u8g2_font_ncenB08_tr);   // choose a suitable font
-  u8g2.drawStr(0,10,"Hello World!");    // write something to the internal memory
-  u8g2.sendBuffer();                    // transfer internal memory to the display
+  u8g2.clearBuffer();                  // clear the internal memory
+  u8g2.setFont(u8g2_font_ncenB08_tr);  // choose a suitable font
+  u8g2.setCursor(0, 10);              // Set cursor to first line
+  u8g2.print("Hello World!");         // write something to the internal memory
+  u8g2.sendBuffer();                  // transfer internal memory to the display
   delay(1000);  
 }
