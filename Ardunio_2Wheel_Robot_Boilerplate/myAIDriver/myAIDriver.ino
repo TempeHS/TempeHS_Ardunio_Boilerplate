d2a366256463fe839fdc8df47a73b1efdaa8a847
Ben Jones <benpaddlejones@outlook.com>
2023-12-18 17:01:18 +1100

    Correct wiring and library

--- Ardunio_2Wheel_Robot_Boilerplate/myAIDriver/myAIDriver.ino
+++ Ardunio_2Wheel_Robot_Boilerplate/myAIDriver/myAIDriver.ino
@@ -21,14 +21,14 @@
       Serial.println(distanceRanger.read());
 			
       //obj->doSomething(); in this case make the object mrJonesDriving rotate to the right at a speed of 200
-      mrJonesDriving->rotateRight(150); // speed can be between 0-255
+      mrJonesDriving->rotateRight(125); // speed can be between 0-255
 			// wait 2000 milliseconds or 2 seconds
       delay(2000);
-      mrJonesDriving->rotateLeft(150);
+      mrJonesDriving->rotateLeft(125);
 			delay(2000);
       //Make mrJonesDriving drive forward left wheel speed 200 and right wheel speeed 200
-      mrJonesDriving->driveForward(150,150);
+      mrJonesDriving->driveForward(125,125);
       delay(2000);
-      mrJonesDriving->driveBackward(150,150);
+      mrJonesDriving->driveBackward(125,125);
       delay(2000);
 	}
