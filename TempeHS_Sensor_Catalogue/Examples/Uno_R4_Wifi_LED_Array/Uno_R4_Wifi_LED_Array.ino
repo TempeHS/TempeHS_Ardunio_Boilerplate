7d492819ac7e4c3ad667daf5f2950ef5a86307a0
Ben Jones <benpaddlejones@outlook.com>
2024-04-27 08:16:46 +1000

    Add software version display in setup metbod

--- TempeHS_Sensor_Catalogue/Examples/Uno_R4_Wifi_LED_Array/Uno_R4_Wifi_LED_Array.ino
+++ TempeHS_Sensor_Catalogue/Examples/Uno_R4_Wifi_LED_Array/Uno_R4_Wifi_LED_Array.ino
@@ -43,6 +43,19 @@ ArduinoLEDMatrix matrix;
 void setup() {
   Serial.begin(9600);
   matrix.begin();
+
+  matrix.beginDraw();
+  matrix.stroke(0xFFFFFFFF);
+  matrix.textScrollSpeed(50);
+  const char text[] = "v0.0.001";
+  matrix.textFont(Font_4x6);
+  matrix.beginText(0, 1, 0xFFFFFF);
+  matrix.println(text);
+  matrix.endText();
+  matrix.endDraw();
+
+  delay(3000);
+
 }
 
 void loop() {
